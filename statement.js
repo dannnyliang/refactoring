5101d7ba8c3e5e035e3966a6fe96445947c5a017
dannnyliang <youchenliang@gmail.com>
2022-05-14 12:49:08 +0800

    enrich the data layer

--- statement.js
+++ statement.js
@@ -7,11 +7,36 @@ function statement(invoice, plays) {
   function ennchPerformance(aPerformance) {
     const result = Object.assign({}, aPerformance);
     result.play = playFor(result);
+    result.amount = amountfor(result);
     return result;
   }
 
   function playFor(aPerformance) {
     return plays[aPerformance.playID];
+  }
+
+  function amountfor(aPerformance) {
+    let result = 0;
+
+    switch (aPerformance.play.type) {
+      case "tragedy":
+        result = 40000;
+        if (aPerformance.audience > 30) {
+          result += 1000 * (aPerformance.audience - 30);
+        }
+        break;
+      case "comedy":
+        result = 30000;
+        if (aPerformance.audience > 20) {
+          result += 10000 + 500 * (aPerformance.audience - 20);
+        }
+        result += 300 * aPerformance.audience;
+        break;
+      default:
+        throw new Error(`unknown type: ${aPerformance.play.type}`);
+    }
+
+    return result;
   }
 }
 
