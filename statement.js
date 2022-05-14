e6c084b8fcaf7b7a209c0b873db1052a251a4364
dannnyliang <youchenliang@gmail.com>
2022-05-14 10:26:53 +0800

    divide the number inside the `usd` function

--- statement.js
+++ statement.js
@@ -40,7 +40,7 @@ function statement(invoice, plays) {
       style: "currency",
       currency: "USD",
       minimumFractionDigits: 2,
-    }).format(aNumber);
+    }).format(aNumber / 100);
   }
 
   let totalAmount = 0;
@@ -51,13 +51,13 @@ function statement(invoice, plays) {
     volumeCredits += volumeCreditsFor(perf);
 
     // 印出這筆訂單
-    result += `  ${playFor(perf).name}: ${usd(amountfor(perf) / 100)} (${
+    result += `  ${playFor(perf).name}: ${usd(amountfor(perf))} (${
       perf.audience
     } seats)\n`;
     totalAmount += amountfor(perf);
   }
 
-  result += `Amount owed is ${usd(totalAmount / 100)}\n`;
+  result += `Amount owed is ${usd(totalAmount)}\n`;
   result += `You earned ${volumeCredits} credits\n`;
   return result;
 }
