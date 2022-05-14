0054feba016d3e8e3e28689eddd02cbe9c8096d6
dannnyliang <youchenliang@gmail.com>
2022-05-14 12:56:34 +0800

    use the enriched data layer

--- statement.js
+++ statement.js
@@ -63,18 +63,10 @@ function renderPlainText(data, plays) {
   result += `You earned ${totalVolumeCredits()} credits\n`;
   return result;
 
-  function volumeCreditsFor(aPerformance) {
-    let result = 0;
-    result += Math.max(aPerformance.audience - 30, 0);
-    if ("comedy" === aPerformance.play.type)
-      result += Math.floor(aPerformance.audience / 5);
-    return result;
-  }
-
   function totalVolumeCredits() {
     let result = 0;
     for (let perf of data.performances) {
-      result += volumeCreditsFor(perf);
+      result += perf.volumeCredits;
     }
     return result;
   }
