bc67fa15ce4605cc35b5ade64232070200dcd1af
dannnyliang <youchenliang@gmail.com>
2022-05-14 15:59:47 +0800

    add html version

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
-const { statement } = require("./statement");
+const { statement, htmlStatement } = require("./statement");
 const play = require("./fixture/play.json");
 const invoices = require("./fixture/invoices.json");
 
 const statementResult = statement(invoices[0], play);
 console.log(statementResult);
+
+const htmlStatementResult = htmlStatement(invoices[0], play);
+console.log(htmlStatementResult);
