49bcf6bd825289463ca0b0faf85be6d00826988f
dannnyliang <youchenliang@gmail.com>
2022-05-14 15:48:29 +0800

    move function `usd` out to function `renderPlainText`

--- statement.js
+++ statement.js
@@ -18,13 +18,14 @@ function renderPlainText(data) {
   result += `You earned ${data.totalVolumeCredits} credits\n`;
   return result;
 
-  function usd(aNumber) {
-    return new Intl.NumberFormat("en-US", {
-      style: "currency",
-      currency: "USD",
-      minimumFractionDigits: 2,
-    }).format(aNumber / 100);
-  }
+}
+
+function usd(aNumber) {
+  return new Intl.NumberFormat("en-US", {
+    style: "currency",
+    currency: "USD",
+    minimumFractionDigits: 2,
+  }).format(aNumber / 100);
 }
 
 module.exports = { statement };
