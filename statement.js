80490209b184030d05dae3523a4f4c532f3bd1e0
dannnyliang <youchenliang@gmail.com>
2022-05-14 12:50:09 +0800

    use the enrich data layer

--- statement.js
+++ statement.js
@@ -45,7 +45,7 @@ function renderPlainText(data, plays) {
 
   for (let perf of data.performances) {
     // 印出這筆訂單
-    result += `  ${perf.play.name}: ${usd(amountfor(perf))} (${
+    result += `  ${perf.play.name}: ${usd(perf.amount)} (${
       perf.audience
     } seats)\n`;
   }
@@ -53,30 +53,6 @@ function renderPlainText(data, plays) {
   result += `Amount owed is ${usd(totalAmount())}\n`;
   result += `You earned ${totalVolumeCredits()} credits\n`;
   return result;
-
-  function amountfor(aPerformance) {
-    let result = 0;
-
-    switch (aPerformance.play.type) {
-      case "tragedy":
-        result = 40000;
-        if (aPerformance.audience > 30) {
-          result += 1000 * (aPerformance.audience - 30);
-        }
-        break;
-      case "comedy":
-        result = 30000;
-        if (aPerformance.audience > 20) {
-          result += 10000 + 500 * (aPerformance.audience - 20);
-        }
-        result += 300 * aPerformance.audience;
-        break;
-      default:
-        throw new Error(`unknown type: ${aPerformance.play.type}`);
-    }
-
-    return result;
-  }
 
   function volumeCreditsFor(aPerformance) {
     let result = 0;
@@ -97,7 +73,7 @@ function renderPlainText(data, plays) {
   function totalAmount() {
     let result = 0;
     for (let perf of data.performances) {
-      result += amountfor(perf);
+      result += perf.amount;
     }
     return result;
   }
