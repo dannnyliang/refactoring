aaae0063a8ff1410bf3473281a38b1b72ffe0202
dannnyliang <youchenliang@gmail.com>
2022-06-03 11:07:38 +0800

    add test case

--- Province.test.js
+++ Province.test.js
@@ -5,7 +5,7 @@ describe("province", function () {
 
   beforeEach(function () {
     asia = new Province(sampleProvinceData());
-  })
+  });
 
   it("shortfall", function () {
     expect(asia.shortfall).toBe(5);
@@ -13,5 +13,11 @@ describe("province", function () {
 
   it("profit", function () {
     expect(asia.profit).toBe(230);
+  });
+
+  it("change production", function () {
+    asia.producers[0].production = 20;
+    expect(asia.shortfall).toBe(-6);
+    expect(asia.profit).toBe(292);
   });
 });
