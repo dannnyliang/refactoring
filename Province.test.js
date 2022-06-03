d9c46609783abf0b472ab275a7258fa016597250
dannnyliang <youchenliang@gmail.com>
2022-06-03 10:51:46 +0800

    create fixture in beforeEach

--- Province.test.js
+++ Province.test.js
@@ -1,13 +1,17 @@
 const { Province, sampleProvinceData } = require("./Province");
 
 describe("province", function () {
+  let asia;
+
+  beforeEach(function () {
+    asia = new Province(sampleProvinceData());
+  })
+
   it("shortfall", function () {
-    const asia = new Province(sampleProvinceData());
     expect(asia.shortfall).toBe(5);
   });
 
   it("profit", function () {
-    const asia = new Province(sampleProvinceData());
     expect(asia.profit).toBe(230);
   });
 });
